453fa96dd6a81e2a4951d3e6c28df2af57a00d29
H̨͉̠͍̹̋̇ͦ͌̒i͇̯̜̩̞̠̒̅̈ͭ́ͬm̭̬͉̬̝͊ͧ̓̊͗̏͑ͅ <cliffatato@gmail.com>
2017-10-26 16:18:25 -0700

    .

---
+++ app/src/main/java/ru/rasb0t/snapnotesbeta/SaveNotesActivity.java
@@ -0,0 +1,17 @@
+package ru.rasb0t.snapnotesbeta;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Crimson on 2017-10-26.
+ */
+
+public class SaveNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
